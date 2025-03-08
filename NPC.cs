abf1e36675665435c22647896906c480a28919e6
LuckyFlamingos <fum3i57j@gmail.com>
2025-03-08 18:06:12 +0300

    Add files via upload

---
+++ NPC.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    //Здоровья NPC
+    public int health = 5;
+
+    //Уровень NPC
+    public int level = 1;
+
+    //Скорость NPC
+    public float speed = 1.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health += level;
+        print("Здоровья NPC: "+ health);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = transform.position;
+        newPosition.z += speed * Time.deltaTime;
+        transform.position = newPosition;
+    }
+}
